2e2116617c745bbd2d051e5d1d25d0354c5f8bd4
Vedanho <vedanho@Vedanhos-MacBook-Pro.local>
2025-05-30 14:45:15 +0300

    relize

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,7 +7,6 @@ import Logo from "/home-page/gold-logo.png";
 
 import "./index.scss";
 
-
 const HomePage = () => {
   return (
     <div className="tap-screen">
@@ -18,19 +17,21 @@ const HomePage = () => {
           <img src={ProgressBar} alt="progress" className="progress-bar__progress" />
           <span className="progress-bar__points">140 000 / 300 000</span>
         </div>
-        <div className="tap-screen__hero">
-          <Hero />
-        </div>
-        <div className="tap-screen__bottom-content">
-          <div className="tap-screen__energy-block">
-            <img src={EnergyIcon} alt="energy" />
-            <div className="tap-screen__energy">
-              <span className="tap-screen__title">Энергия</span>
-              <span className="tap-screen__energy-count">7600/8000</span>
-            </div>
+        <div className="tap-screen__content">
+          <div className="tap-screen__hero">
+            <Hero />
           </div>
-          <div className="tap-screen__logo">
-            <img src={Logo} alt="logo" />
+          <div className="tap-screen__bottom-content">
+            <div className="tap-screen__energy-block">
+              <img src={EnergyIcon} alt="energy" />
+              <div className="tap-screen__energy">
+                <span className="tap-screen__title">Энергия</span>
+                <span className="tap-screen__energy-count">7600/8000</span>
+              </div>
+            </div>
+            <div className="tap-screen__logo">
+              <img src={Logo} alt="logo" />
+            </div>
           </div>
         </div>
       </div>
