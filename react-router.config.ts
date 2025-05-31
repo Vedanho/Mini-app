24fd5eb17a02f13d80cd6e979b4fdc30912cb182
Vedanho <vedanho@Vedanhos-MacBook-Pro.local>
2025-05-31 22:35:34 +0300

    relise

---
+++ react-router.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "@react-router/dev/config";
+
+export default {
+  ssr: false, // disable runtime server rendering
+  prerender: true, // pre-render all static routes
+
+} satisfies Config;
--- src/Layout/AnimatedLayout/index.tsx
+++ src/Layout/AnimatedLayout/index.tsx
@@ -1,21 +1,63 @@
 import { useOutlet, useLocation } from "react-router";
 import { AnimatePresence, motion } from "framer-motion";
+import { Pages } from "../../pages";
+import { useEffect, useRef, useState } from "react";
+
+const PagesOrder = [Pages.main, Pages.shop, Pages.rating, Pages.discount];
 
 export default function AnimatedOutlet() {
   const outlet = useOutlet();
   const location = useLocation();
+  const ref = useRef(location.pathname);
+  const currentPageOrder = PagesOrder.indexOf(location.pathname);
+
+  const [direction, setDirection] = useState<number | string>("right");
+  useEffect(() => {
+    const nextPageOrder = PagesOrder.indexOf(ref.current);
+    if (currentPageOrder > nextPageOrder) {
+      setDirection("left");
+    } else {
+      setDirection("right");
+    }
+
+    ref.current = location.pathname;
+  }, [location.pathname]);
+
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    if (typeof args[0] === "string" && args[0].startsWith("[Telegram.WebView] > postEvent")) {
+      // игнорируем эти логи
+      return;
+    }
+    originalConsoleLog(...args);
+  };
+
+  console.log("ref", ref);
+  const variants = {
+    initial: () => {
+      return {
+        x: direction === "left" ? 300 : -300,
+        opacity: 0,
+      };
+    },
+    animate: { x: 0, opacity: 1,  },
+  };
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={location.pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 1 }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-      >
-        {outlet}
-      </motion.div>
-    </AnimatePresence>
+    <div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          custom={currentPageOrder}
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          {outlet}
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 }
